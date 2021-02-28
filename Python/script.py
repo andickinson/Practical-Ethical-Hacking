06092fcd917315d7a76d019315c4449ba10b97a9
Andrew <andickinson@gmail.com>
2021-02-28 22:28:30 +1100

    Added functions lesson

--- Python/script.py
+++ Python/script.py
@@ -23,3 +23,41 @@ print(age)
 birthday = 1
 age += birthday
 print(age)
+
+print('\n')
+# Functions
+print("Here is an example function:")
+
+def who_am_i(): # this is a function
+    name = "Andrew"
+    age = "32"
+    print("My name is " + name + " and I am " + str(age) + " years old.")
+
+who_am_i()
+
+# adding parameters
+def add_one_hundred(num):
+    print(num + 100)
+
+add_one_hundred(100)
+
+# multiple parameters
+def add(x, y):
+    print(x + y)
+
+add(7, 7)
+
+def multiply(x, y):
+    return x * y
+
+print(multiply(7, 7))
+
+def square_root(x):
+    print(x ** 0.5)
+
+square_root(64)
+
+def nl():
+    print('\n')
+
+nl()
