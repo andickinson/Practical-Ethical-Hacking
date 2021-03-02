158eefcfd54facb7ea745188057870cc73eec75a
Andrew <andickinson@gmail.com>
2021-03-02 18:46:35 +1100

    Advanced Strings

--- Python/new-script.py
+++ Python/new-script.py
@@ -4,3 +4,31 @@ import sys # system functions and parameters
 from datetime import datetime as dt # import with alias
 
 print(dt.now())
+
+my_name = "Andrew"
+print(my_name[0])
+print(my_name[-1])
+
+sentence = "This is a sentence."
+print(sentence[:4])
+
+print(sentence.split())
+
+sentence_split = sentence.split()
+sentence_join = ' '.join(sentence_split)
+print(sentence_join)
+
+quote = "He said, \"give me all your money\""
+print(quote)
+
+too_much_space = "                     hello                     "
+print(too_much_space.strip())
+
+print("a" in "Apple")
+
+letter = "A"
+word = "Apple"
+print(letter.lower() in word.lower())
+
+movie = "The Hangover"
+print("My favorite movie is {}.".format(movie))
